426fa740a4e454ff1ebaa9c12a80b9cbbbbfe671
Saugat Shahi <dev.shahi.apps@gmail.com>
2026-04-27 11:19:46 +0545

    Initial commit

---
+++ src/main/java/com/cloudlens/server/CloudlensApplication.java
@@ -0,0 +1,19 @@
+package com.cloudlens.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CloudlensApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CloudlensApplication.class, args);
+  }
+
+  @GetMapping("/api/health")
+  public String checkHealth() {
+    return "Good";
+  }
+}
