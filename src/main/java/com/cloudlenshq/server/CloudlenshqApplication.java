549b5987453a5c6f896315b9cd100242367bc287
Saugat Shahi <dev.shahi.apps@gmail.com>
2026-05-14 13:21:22 +0545

    add: docker compose and deployment config add

--- src/main/java/com/cloudlenshq/server/CloudlenshqApplication.java
+++ src/main/java/com/cloudlenshq/server/CloudlenshqApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudlenshqApplication {
   public static void main(String[] args) {
+    try {
+      io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.configure()
+          .ignoreIfMissing()
+          .load();
+      dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    } catch (Exception e) {
+    }
+    
     SpringApplication.run(CloudlenshqApplication.class, args);
   }
 }
